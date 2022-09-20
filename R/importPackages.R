389a500392b1b64d2c693a7651d772dd7c49230f
Luca Del Core <l.del.core@rug.nl>
2022-09-20 09:46:14 +0000

    version 1.0

---
+++ R/importPackages.R
@@ -0,0 +1,10 @@
+#' @import Matrix
+#' @import xtable
+#' @import scales
+#' @import stringr
+#' @import ggplot2
+#' @import scatterpie
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom graphics boxplot
+#' @importFrom stats runif rnorm optim pchisq
+NULL
